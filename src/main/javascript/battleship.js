176978c5ffea8298a0f17fe6299307618d251ec2
jtkline1205 <jtkline1205@gmail.com>
2018-07-04 16:56:03 -0400

    continuing battleship work

--- src/main/javascript/battleship.js
+++ src/main/javascript/battleship.js
@@ -8,17 +8,12 @@ function BattleshipCtrl() {
         ctrl.redWins = 0;
         ctrl.blueWins = 0;
         ctrl.showShipGrids = false;
-        ctrl.firstTeam = "";
-        ctrl.currentTeam = "blue";
+        ctrl.currentTeam = "red";
         ctrl.message = "Welcome to Battleship";
     }
 
     this.toggleShipGrids = function() {
         this.showShipGrids = !this.showShipGrids;
-    }
-
-    this.revealButton = function($scope, square) {
-        $scope.clicked = true;
     }
 
     function switchTeams(ctrl) {
@@ -29,56 +24,12 @@ function BattleshipCtrl() {
         }
     }
 
-    // function resolveGuess(word, ctrl) {
-    //     if (word.team == 'red') {
-    //         ctrl.redLeft--;
-    //         if (ctrl.currentTeam == 'red') {
-    //             ctrl.guessesRemaining--;
-    //         } else {
-    //             switchTeams(ctrl);
-    //         }
-    //     } else if (word.team == 'blue') {
-    //         ctrl.blueLeft--;
-    //         if (ctrl.currentTeam =='blue') {
-    //             ctrl.guessesRemaining--;
-    //         } else {
-    //             switchTeams(ctrl);
-    //         }
-    //     } else if (word.team == 'brown') {
-    //         switchTeams(ctrl);
-    //     } else if (word.team == 'black') {
-    //         if (ctrl.currentTeam == 'red') {
-    //             ctrl.blueWins++;
-    //             ctrl.message = "Blue wins since red guessed the black card!";
-    //         } else if (ctrl.currentTeam == 'blue') {
-    //             ctrl.redWins++;
-    //             ctrl.message = "Red wins since blue guessed the black card!";
-    //         }
-    //         ctrl.startNewRound();
-    //     }
-    //     if (ctrl.redLeft == 0) {
-    //         ctrl.redWins++;
-    //         ctrl.startNewRound();
-    //         ctrl.message = "Red wins by guessing all words!";
-    //     } else if (ctrl.blueLeft == 0) {
-    //         ctrl.blueWins++;
-    //         ctrl.startNewRound();
-    //         ctrl.message = "Blue wins by guessing all words!";
-    //     } else if (ctrl.guessesRemaining == 0) {
-    //         switchTeams(ctrl);
-    //     }
-    // }
-
     this.startNewRound = function() {
-        this.player1RowList = generateRowList(this, 10);
-        this.player2RowList = generateRowList(this, 10);
+        this.player1RowList = generateRowList(this, 10, "red");
+        this.player2RowList = generateRowList(this, 10, "blue");
     }
 
-    function generateRowList(ctrl, gridSize) {
-        // let firstTeamIndex = Math.floor(Math.random()*2);
-        // colorFreqs[firstTeamIndex]++;
-        // ctrl.firstTeam = colorNames[firstTeamIndex];
-        // ctrl.currentTeam = colorNames[firstTeamIndex];
+    function generateRowList(ctrl, gridSize, team) {
         let playerGrid = generateShipGrid(gridSize);
         let playerRowList = [];
         let colors = ["blue", "red", "orange", "yellow", "green", "purple"];
@@ -89,6 +40,7 @@ function BattleshipCtrl() {
             let col = 0;
             while (col < gridSize) {
                 playerRowList[row].push({
+                    "team":team,
                     "type":colors[playerGrid[row][col]],
                     "status":"notFiredOn"
                 })
@@ -101,44 +53,41 @@ function BattleshipCtrl() {
     }
 
     this.fireOn = function($scope, square) {
-        if (square.status=="notFiredOn") {
+        if (square.status=="notFiredOn" && square.team!=this.currentTeam) {
             if (square.type != "blue") {
                 square.status = "hitShip";
             } else {
                 square.status = "missedShip";
             }
+            switchTeams(this);
         }
     }
 
     this.startNewRound();
 
     function generateShipGrid(gridSize) {
-        let grid = [
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        ]
-        placeShip(grid, gridSize, 5, 1);
-        placeShip(grid, gridSize, 4, 2);
-        placeShip(grid, gridSize, 3, 3);
-        placeShip(grid, gridSize, 3, 4);
-        placeShip(grid, gridSize, 2, 5);
+        let grid = [];
+        for (let i=0; i<gridSize; i++) {
+            let newRow = [];
+            for (let i = 0; i < gridSize; i++) {
+                newRow.push(0);
+            }
+            grid.push(newRow);
+        }
+        placeShip(grid, 5, 1);
+        placeShip(grid, 4, 2);
+        placeShip(grid, 3, 3);
+        placeShip(grid, 3, 4);
+        placeShip(grid, 2, 5);
         return grid;
     }
 
-    function placeShip(grid, gridSize, shipSize, shipColor) {
+    function placeShip(grid, shipSize, shipColor) {
         let direction = Math.floor(Math.random()*2);
-        let bound = gridSize-shipSize+1;
+        let bound = grid.length-shipSize+1;
         if (direction==0) {
             //horizontal
-            let shipStartRow = Math.floor(Math.random()*gridSize);
+            let shipStartRow = Math.floor(Math.random()*grid.length);
             let shipStartCol = Math.floor(Math.random()*bound);
             let crossedShip = true;
             while (crossedShip) {
@@ -147,7 +96,7 @@ function BattleshipCtrl() {
                     if (grid[shipStartRow][shipStartCol+i]!=0) {
                         crossedShip = true;
                         i=shipSize;
-                        shipStartRow = Math.floor(Math.random()*gridSize);
+                        shipStartRow = Math.floor(Math.random()*grid.length);
                         shipStartCol = Math.floor(Math.random()*bound);
                     }
                 }
@@ -158,7 +107,7 @@ function BattleshipCtrl() {
         } else if (direction==1) {
             //vertical
             let shipStartRow = Math.floor(Math.random()*bound);
-            let shipStartCol = Math.floor(Math.random()*gridSize);
+            let shipStartCol = Math.floor(Math.random()*grid.length);
             let crossedShip = true;
             while (crossedShip) {
                 crossedShip = false;
@@ -167,7 +116,7 @@ function BattleshipCtrl() {
                         crossedShip = true;
                         i=shipSize;
                         shipStartRow = Math.floor(Math.random()*bound);
-                        shipStartCol = Math.floor(Math.random()*gridSize);
+                        shipStartCol = Math.floor(Math.random()*grid.length);
                     }
                 }
             }
@@ -177,8 +126,4 @@ function BattleshipCtrl() {
         }
         return grid;
     }
-
-    
-
 }
-
